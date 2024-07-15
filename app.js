2d25675bdbb295ea4321a6f202707d7cb11a8caa
Gyanendra <gyanen56@gmail.com>
2024-07-15 19:33:21 +1000

    fixing css

--- app.js
+++ app.js
@@ -6,7 +6,8 @@ const __dirname = path.resolve()
 
 // static files
 app.use(express.static(path.join(__dirname, 'public')))
-app.get("/hello", (req,res,next)=>{
+//home page
+app.get("/", (req,res,next)=>{
     // res.send("hello word")
     res.sendFile(
         path.join(__dirname,"src","index.html")
@@ -15,6 +16,14 @@ app.get("/hello", (req,res,next)=>{
     console.log(next)
 })
 
+// login
+app.get("/login",(req,res)=>{
+    res.sendFile(path.join(__dirname,"src","login.html"))
+})
+//register
+app.get("/register",(req,res)=>{
+    res.sendFile(path.join(__dirname,"src","register.html"))
+})
 app.listen(PORT, (error)=>{
     error? console.log(error): console.log(`app is listening ${PORT}` )
 })
