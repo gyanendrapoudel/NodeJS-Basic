112dbd3781f50653e7b0c8d6d1ee035848942e14
Gyanendra <gyanen56@gmail.com>
2024-07-16 10:49:43 +1000

    register form submit with post method

--- app.js
+++ app.js
@@ -6,6 +6,9 @@ const __dirname = path.resolve()
 
 // static files
 app.use(express.static(path.join(__dirname, 'public')))
+// middleware
+app.use(express.json())
+app.use(express.urlencoded())
 //home page
 app.get("/", (req,res,next)=>{
     // res.send("hello word")
@@ -23,6 +26,12 @@ app.get("/login",(req,res)=>{
 //register
 app.get("/register",(req,res)=>{
     res.sendFile(path.join(__dirname,"src","register.html"))
+})
+// after submitting with post method
+app.post("/register",(req,res)=>{
+ console.log('request body data',req.body)
+ console.log('request query data',req.query)
+ res.send("data submitted")
 })
 app.listen(PORT, (error)=>{
     error? console.log(error): console.log(`app is listening ${PORT}` )
