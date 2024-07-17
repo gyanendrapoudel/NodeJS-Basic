e0a0f1ad1ab4ac77d909583f9748ecb3410ffe8d
Gyanendra <gyanen56@gmail.com>
2024-07-17 11:32:45 +1000

    reading csv file and displaying on home page

--- app.js
+++ app.js
@@ -4,40 +4,71 @@ const app = express();
 const PORT = 8000;
 const __dirname = path.resolve()
 import fs from 'fs'
-
+const fileName = 'userlist.csv'
+import { htmlGenerator } from './src/htmlGenerator.js';
 // static files
 app.use(express.static(path.join(__dirname, 'public')))
 // middleware
+
 app.use(express.json())
 app.use(express.urlencoded())
+
 //home page
 app.get("/", (req,res,next)=>{
     // res.send("hello word")
-    res.sendFile(
-        path.join(__dirname,"src","index.html")
-    //   `${__dirname}/src/index.html`
-    )
-    console.log(next)
+    
+    fs.readFile(fileName, 'utf-8',(error,data)=>{
+        if(error){
+            console.log(error)
+        }else{
+            
+           
+            
+
+          res.send( htmlGenerator( data.split("\n")))
+        }
+    })
+
+    // res.sendFile(
+    //     path.join(__dirname,"src","index.html")
+    // //   `${__dirname}/src/index.html`
+    // )
+    
+    
 })
+
 
 // login
 app.get("/login",(req,res)=>{
     res.sendFile(path.join(__dirname,"src","login.html"))
+
+})
+app.post('/login',(req,res)=>{
+    const {email, password} = req.body
+    
 })
 //register
 app.get("/register",(req,res)=>{
     res.sendFile(path.join(__dirname,"src","register.html"))
 })
-const fileName = 'userlist.csv'
+
 // after submitting with post method
 app.post("/register",(req,res)=>{
  console.log('request body data',req.body)
  console.log('request query data',req.query)
  const {name, password, email}= req.body
- const str = `${name},${password},${email}`
- console.log(str)
- console.log(str.split(','))
- fs.appendFile(fileName,str,(error)=>{error?console.log(error):res.send("user registered")})
+ 
+ const str = `${name},${email},${password}\n`
+ fs.appendFile(fileName,str,(error)=>{
+    if(error){
+console.log(error)
+    }else{
+        
+        res.redirect('/')
+       
+    }
+  
+})
 })
 app.listen(PORT, (error)=>{
     error? console.log(error): console.log(`app is listening ${PORT}` )
--- src/htmlGenerator.js
+++ src/htmlGenerator.js
@@ -1,11 +1,15 @@
-<!DOCTYPE html>
+export const htmlGenerator=(data)=>{
+    console.log(data)
+    const dataHTML = data.reduce((acc,user)=> user.length?acc+`<li>${user.split(',')[0]}: ${user.split(',')[1]}</li>`:acc,'')
+    console.log(dataHTML)
+    return `<!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <link rel="stylesheet" href="style.css">
-    <title>Docuent</title>
+    <title>Document</title>
     <style>
        
     </style>
@@ -21,9 +25,14 @@
     </header>
    
         <div class="alert alert-primary w-50 m-auto mt-5" role="alert">
-        Welcome to the community! Stay Connected
+        Welcome to the community! Stay Connected user lists
+        <ul>`
+        +dataHTML+
+        `</ul>
         </div>
     
     
 </body>
-</html>
+</html>`
+}
+
